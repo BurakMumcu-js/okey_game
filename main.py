b1117563c26d40d2cb57dea4ffb3fbdc5c1ff7a5
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-01-16 04:34:03 +0300

    feat:new project uploading github

---
+++ main.py
@@ -0,0 +1,38 @@
+# okey taşları oluşturup dağıtan fonksiyon
+import random
+
+renkler = ["sarı", "mavi", "kırmızı", "siyah"]
+oyuncular = {"burak": [], "bekir": [], "merve": [], "Ayda": []}
+taslar = []
+okey = None
+def taslarıOlustur():
+    taslar.append(["sahte"])
+    taslar.append(["sahte"])
+    for renk in renkler:
+        for i in range(1, 14):
+            taslar.extend([[renk, i]] * 2)  # bu kod iki kez eklenmesini sağlar
+
+def okeyOlustur():
+    okey = random.choice(taslar)
+    taslar.remove(okey)
+    print(okey)
+
+def taslariDagit(oyuncu):
+   for i in range(21):
+     tas = random.choice(taslar)
+     oyuncu.append(tas)
+     taslar.remove(tas)
+   print(oyuncu)
+
+def main():
+
+    taslarıOlustur()
+    okeyOlustur()
+    taslariDagit(oyuncular["burak"])
+    taslariDagit(oyuncular["bekir"])
+    taslariDagit(oyuncular["merve"])
+    taslariDagit(oyuncular["Ayda"])
+
+
+if __name__ == "__main__":
+    main()
